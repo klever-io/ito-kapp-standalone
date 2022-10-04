7c2c20c0da3649c652d7e5145abf81e933124b9b
Diego Cândido <diegocndd4@gmail.com>
2022-10-04 18:32:40 -0300

    fixed range price for 1 and 2 packs

--- src/components/FungibleITO/index.tsx
+++ src/components/FungibleITO/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
 import {
   Container,
@@ -21,37 +22,46 @@ interface IFungibleITO {
 const FungibleITO: React.FC<IFungibleITO> = ({ asset }) => {
   const [amount, setAmount] = useState(0);
 
-  const calculateCost = (indexPackData: number) => {
-    let priceIndex;
-    const range: number[] = [];
-
-    asset.ito?.packData[indexPackData].packs.forEach((pack: IPack) => {
-      range.push(pack.amount);
-    });
-
-    for (const rangeIndex in range) {
-      if (amount < range[rangeIndex]) {
-        priceIndex = rangeIndex;
-        break;
+  const calculateCost = (indexPackData: number, qtyPacks: number) => {
+    if (asset.ito) {
+      const packs = asset.ito.packData[indexPackData].packs;
+      if (qtyPacks === 1) {
+        return amount * packs[0].price;
+      } else if (qtyPacks === 2) {
+        if (amount >= 0 && amount <= packs[0].amount) {
+          return amount * packs[0].price;
+        } else if (amount >= packs[1].price) {
+          return amount * packs[1].price;
+        }
       }
-    }
 
-    let cost = 0;
+      let priceIndex;
+      const range: number[] = [];
 
-    if (!priceIndex) {
-      if (asset.ito?.packData[indexPackData].packs) {
-        priceIndex = asset.ito?.packData[indexPackData].packs.length - 1;
-        cost =
-          amount * asset.ito?.packData[indexPackData].packs[priceIndex].price;
+      packs.forEach((pack: IPack) => {
+        range.push(pack.amount);
+      });
+
+      for (const rangeIndex in range) {
+        if (amount <= range[rangeIndex]) {
+          priceIndex = rangeIndex;
+          break;
+        }
       }
-    } else {
-      cost =
-        amount * asset.ito?.packData[indexPackData].packs[priceIndex].price;
-    }
 
-    return isFloat(cost) && String(cost).length > 10
-      ? cost.toPrecision(5)
-      : cost;
+      let cost = 0;
+
+      if (!priceIndex) {
+        priceIndex = packs.length - 1;
+        cost = amount * packs[priceIndex].price;
+      } else {
+        cost = amount * packs[priceIndex].price;
+      }
+
+      return isFloat(cost) && String(cost).length > 10
+        ? cost.toPrecision(5)
+        : cost;
+    }
   };
 
   const handleSubmit = (currencyId: string) => {
@@ -80,7 +90,7 @@ const FungibleITO: React.FC<IFungibleITO> = ({ asset }) => {
               <TotalPrice>
                 <span>You will pay</span>{' '}
                 <span>
-                  {calculateCost(indexPackData)} {pack.key}
+                  {calculateCost(indexPackData, pack.packs.length)} {pack.key}
                 </span>
               </TotalPrice>
               <Button onClick={() => handleSubmit(pack.key)}>
@@ -94,14 +104,42 @@ const FungibleITO: React.FC<IFungibleITO> = ({ asset }) => {
                   const initialValue =
                     index === 0 ? 0 : pack.packs[index - 1].amount;
                   const isLastElement = pack.packs.length === index + 1;
+
+                  if (pack.packs.length === 1) {
+                    return (
+                      <Row>
+                        <span>0 +</span>
+                        <span>
+                          {packInfo.price} {pack.key} / {asset.ticker}
+                        </span>
+                      </Row>
+                    );
+                  } else if (pack.packs.length === 2) {
+                    return (
+                      <Row>
+                        <span>
+                          {index === 0
+                            ? `0 - ${packInfo.amount}`
+                            : `${pack.packs[0].amount + 1} +`}
+                        </span>
+                        <span>
+                          {packInfo.price} {pack.key} / {asset.ticker}
+                        </span>
+                      </Row>
+                    );
+                  }
+
                   return (
                     <Row>
-                      {isLastElement && index > 0 ? (
-                        <span>{packInfo.amount}+</span>
-                      ) : (
+                      {index === 0 && <span>0 - {packInfo.amount}</span>}
+                      {!isLastElement && index > 0 && (
                         <span>
-                          {initialValue} - {packInfo.amount - 1}
+                          {pack.packs[index - 1].amount + 1} -{' '}
+                          {pack.packs[index].amount}
                         </span>
+                      )}
+                      {isLastElement && (
+                        <span>{pack.packs[index].amount} +</span>
                       )}
                       <span>
                         {packInfo.price} {pack.key} / {asset.ticker}
